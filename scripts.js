dee838b78de40e8c5e7919127f46629e67816f90
IvanMusienko <musienk20091@gmail.com>
2024-10-08 11:47:56 +0300

    with_entered_numbers

--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
 let randomNum = null;
 let userInput = null;
 let counter = 0;
+let entered_numbers;
 
 function OnLoad(){
     randomNum = Math.floor(Math.random() * 100);
-    console.log(randomNum);
 }
 
 function CheckAnswer(){
     userInput = document.getElementById('mainInput').value;
+    entered_numbers = document.getElementById('entered_numbers')
     counter++;
+    entered_numbers.innerHTML += ` ${userInput},`;
     document.getElementById("tryCount").textContent=`Число попыток: ${counter}`;
     if(userInput == randomNum){
         document.getElementById("hint").textContent="Подсказка: В точку";
@@ -27,6 +29,5 @@ function CheckAnswer(){
         document.getElementById("hint").textContent="Подсказка: Вы ввели не число!";
         document.getElementById("result").textContent="Результат: Попробуйте еще раз";
     }
-    console.log(userInput);
     document.getElementById('mainInput').value = '';
 }
